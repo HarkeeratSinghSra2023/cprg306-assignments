0899c454a8c6a75238d7bf7617e8fb40b61ba100
HarkeeratSinghSra2023 <harkeeratsingh.sra@edu.sait.ca>
2024-09-24 18:51:27 -0600

    Week 2 complete

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function StudentInfo() {
+    return(
+        <main>
+            <h1>Name: Harkeerat Singh</h1>
+            <p>Github Repository: <a href='https://github.com/HarkeeratSinghSra2023/cprg306-assignments.git'>cprg306-assignments</a></p>
+        </main>
+    )
+}
+
+export default StudentInfo;
