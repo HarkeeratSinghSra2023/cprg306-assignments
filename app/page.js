f7d0699e3a00b3f39f1ed6e93a12ed1e789da0a0
HarkeeratSinghSra2023 <harkeeratsingh.sra@edu.sait.ca>
2024-10-01 19:54:00 -0600

    Week 3 complete

--- app/page.js
+++ app/page.js
@@ -2,7 +2,8 @@ export default function Page() {
   return(
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <a href="./week-2"> week - 2 assignment</a>
+      <a href="./week-2"> week - 2 assignment</a><br/>
+      <a href="./week-3"> week - 3 assignment</a>
     </main>
   );
 }
